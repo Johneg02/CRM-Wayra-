d0a1b15462d7029a155d0b9945e2b6ea6e23caa5
Johneg02 <jonathanegarciasilveira@gmail.com>
2020-01-17 22:42:08 +0000

    navbar modificado

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -3,15 +3,37 @@ import { Link } from "react-router-dom";
 
 export const Navbar = () => {
 	return (
-		<nav className="navbar navbar-light bg-light mb-3">
-			<Link to="/">
-				<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-			</Link>
-			<div className="ml-auto">
-				<Link to="/demo">
-					<button className="btn btn-primary">Check the Context in action</button>
-				</Link>
+		<div className="container-1" id="containerNavbar">
+			<div className="cell-1" id="containerCell1">
+				<button className="buttonNav" id="btinicio">
+					Inicio
+				</button>
+				<button className="buttonNav" id="btleads">
+					Leads
+				</button>
+				<button className="buttonNav" id="btcalendario">
+					Calendario
+				</button>
+				<button className="buttonNav" id="btmetricas">
+					Métricas
+				</button>
+				<button className="buttonNav" id="btinformes">
+					Informes
+				</button>
 			</div>
-		</nav>
+
+			<div className="cell-2" id="containerCell2">
+				<div className="dropdown">
+					<button class="dropbtn" id="btdropdownnav">
+						Opciones
+					</button>
+					<div className="dropdown-content" id="dropdown-content">
+						<a href="#">Link 1</a>
+						<a href="#">Link 2</a>
+						<a href="#">Link 3</a>
+					</div>
+				</div>
+			</div>
+		</div>
 	);
 };
